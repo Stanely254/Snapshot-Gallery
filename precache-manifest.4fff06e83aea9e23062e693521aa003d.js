d12f043b03dcb558d70fea99404de81b6b0d66fa
Stanely Muyuga <stanelymyuga.12@gmail.com>
2020-08-21 18:39:29 +0300

    Updates

---
+++ precache-manifest.4fff06e83aea9e23062e693521aa003d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9e7cc0a4e008b006c2e2216b3dde24",
+    "url": "/Snapshot-Gallery/index.html"
+  },
+  {
+    "revision": "a8efb7ab33e96fb7cf34",
+    "url": "/Snapshot-Gallery/static/css/main.708af5c2.chunk.css"
+  },
+  {
+    "revision": "c41ec4f59335dbe99aef",
+    "url": "/Snapshot-Gallery/static/js/2.8b8dd4be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Snapshot-Gallery/static/js/2.8b8dd4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8efb7ab33e96fb7cf34",
+    "url": "/Snapshot-Gallery/static/js/main.2209bc8d.chunk.js"
+  },
+  {
+    "revision": "c5902f21cc31e9c6bb10",
+    "url": "/Snapshot-Gallery/static/js/runtime-main.b8b1254c.js"
+  }
+]);
